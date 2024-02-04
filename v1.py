79bb27e365b49951aa8b35a392017deea58b251f
Saransh Bhandari <saranshbhandari28@gmail.com>
2024-02-04 16:22:27 +0530

    b1 3st commit in main
    second commit in main
    
    Revert "second commit in main"
    
    This reverts commit fcc306282ba3b5a2f7167139ce5a4f18ce4084a0.
    
    b4 4th commit

--- v1.py
+++ v1.py
@@ -1,6 +1,17 @@
 '''
+<<<<<<< HEAD
 first line == main branch.
 second line == main branch.
 Third line == main branch.------------------------b1 1st commit
                           ------------------------b1 2st commit
+                          ------------------------b1 3st commit
+
+This is the first line in the main branch.
+This is the second line in the main branch.
+This is the Third line in the main branch.
+This is the forth line in the main branch.----------------------b4 1st commit.
+                                          ----------------------b4 2nd commit.
+                                          ----------------------b4 3rd commit.
+                                          ----------------------b4 4th commit.
+>>>>>>> 1e591e6 (b4 4th commit)
 '''
